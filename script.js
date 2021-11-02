56e6d3c98caad900663a5377b65a1a77a9808fdf
viktoor48 <71779943+viktoor48@users.noreply.github.com>
2021-11-02 19:22:11 +0300

    form enter

--- script.js
+++ script.js
@@ -5,8 +5,27 @@ $(document).ready(function() {
         $('.header_burger, .header_menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+    $('.header_link.registration').click(function(event) {
+        $('#popup_reg, .form_registration').addClass('open');
+        $('body').addClass('lock');
+    });
+    $('.close_popup').click(function () {
+        $('#popup_reg, .form_registration').removeClass('open');
+        $('body').removeClass('lock');
+    });
+
+    $('.header_link.enter').click(function(event) {
+        $('#popup_enter, .form_enter').addClass('open');
+        $('body').addClass('lock');
+    });
+    $('.close_popup').click(function () {
+        $('#popup_enter, .form_enter').removeClass('open');
+        $('body').removeClass('lock');
+    });
 });
 
+/*
 var login_btn = document.getElementsByClassName('login');
 var registration_btn = document.getElementsByClassName('registration');
 
@@ -16,4 +35,4 @@ var popup_inner = document.getElementsByClassName('popups_inner');
 registration_btn[0].onclick = function (){
     form[0].classList.add('open');
     popup_inner[0].classList.add('open');
-}
+}*/
