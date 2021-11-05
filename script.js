33a74a69f2c457c6025a841bfe6464cae5c81a21
viktoor48 <71779943+viktoor48@users.noreply.github.com>
2021-11-05 15:14:44 +0300

    validation reg

--- script.js
+++ script.js
@@ -38,11 +38,6 @@ document.addEventListener('DOMContentLoaded', function ()
 
         if (error === 0){
             /*form_registration.classList.add('_sending');*/
-            console.log(form_registration.name.value);
-            console.log(form_registration.email.value);
-            console.log(form_registration.userPhone.value);
-            console.log(form_registration.userPass.value);
-            console.log(form_registration.userPassRepeat.value);
             let response = await fetch('https://httpbin.org/post',{
                 method: 'POST',
                 body: formData
@@ -59,12 +54,9 @@ document.addEventListener('DOMContentLoaded', function ()
                 alert('Ошибка');
                 /*form_registration.classList.remove('_sending');*/
             }
-        }else {
-            alert('Заполните обязательные поля');
         }
     }
     /*Валидация формы регистрации конец*/
-
     function formValidate(form){
         let error = 0;
         let formReq = form.querySelectorAll('._req');
@@ -74,7 +66,6 @@ document.addEventListener('DOMContentLoaded', function ()
             const input = formReq[index];
             formRemoveError(input);
 
-            //нужно осуществить проверку паролей
             if (input.classList.contains('_email')){
                 if (emailTest(input)){
                     formAddError(input);
@@ -83,9 +74,34 @@ document.addEventListener('DOMContentLoaded', function ()
             }else if (input.getAttribute("type") === "checkbox" && input.checked === false){
                 formAddError(input);
                 error++;
-            }else {
-                if (input.value === ''){
+            }else if (input.value === ''){
                     formAddError(input);
+                    error++;
+            }else if (input.classList.contains('_phone')){
+                if (phoneTest(input)){
+                    formAddError(input);
+                    alert('Телефон должен соответствовать данному шаблону +79192569330');
+                    error++;
+                }
+            }else if (input.classList.contains('name')){
+                if (nameTest(input)){
+                    formAddError(input);
+                    alert('Имя должно содержать только кириллицу');
+                    error++;
+                }
+            }else  if (input.classList.contains('_pass')){
+                if (passwordTest(input)){
+                    formAddError(input);
+                    alert('Пароль допускает только от 6 до 16 символов. Также должен содержать по ' +
+                        'крайней мере одной цифры, заглавной или строчной буквы и по крайней ' +
+                        'мере одного специального символа (символов, отличных от букв и цифр).');
+                    error++;
+                }
+            }
+            if (input.classList.contains('_pass')){
+                if (input.value !== formReq[index+1].value){
+                    formAddError(input);
+                    alert('Пароли должны совпадать');
                     error++;
                 }
             }
@@ -103,8 +119,6 @@ document.addEventListener('DOMContentLoaded', function ()
 
         if (error === 0){
             /*form_enter.classList.add('_sending');*/
-            console.log(form_enter.userName.value);
-            console.log(form_enter.userPassword.value);
             let response = await fetch('https://httpbin.org/post',{
                 method: 'POST',
                 body: formData
@@ -112,7 +126,7 @@ document.addEventListener('DOMContentLoaded', function ()
             if (response.ok){
                 let result = await response.json();
                 alert(result.message);
-                console.log(result.value);
+                console.log(result);
                 form_enter.reset();
                 $('#popup_enter, .form_enter').removeClass('open');
                 $('body').removeClass('lock');
@@ -138,6 +152,18 @@ document.addEventListener('DOMContentLoaded', function ()
     //Функция теста email
     function emailTest(input){
         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+    //Функция теста пароля
+    function passwordTest(input){
+        return !/^(?=.*\d)(?=.*?[a-zA-Z])(?=.*?[\W_]).{6,16}$/.test(input.value);
+    }
+    //Функция теста телефона
+    function phoneTest(input){
+        return !/^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/.test(input.value);
+    }
+    //Функция теста имени
+    function nameTest(input){
+        return !/[а-яА-Я]{3,30}/.test(input.value);
     }
 });
 
